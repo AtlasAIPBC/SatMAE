6c474c317415369836b38cefd38c493c1f858ed0
ada-k <adakibetj@protonmail.com>
2023-10-18 06:19:23 +0000

    single image classification script using finetune checkpoint

---
+++ evaluate_single_image_classification_finetune.sh
@@ -0,0 +1,12 @@
+python -m torch.distributed.launch --nproc_per_node=8 \
+    --nnodes=1 --master_port=1234 main_finetune.py \
+    --batch_size 16 \
+    --output_dir /home/ada/satmae/temporal/evaluation/single_image_classification/finetune \
+    --log_dir /home/ada/satmae/temporal/evaluation/single_image_classification/finetune \
+    --device cpu \
+    --model vit_large_patch16 \
+    --resume /home/ada/satmae/temporal/checkpoints/fmow_finetune.pth \
+    --dist_eval --eval --num_workers 8 --dataset rgb \
+    --train_path /home/ada/satmae/temporal/preprocessed/fmow/train/train_62classes.csv \
+    --test_path /home/ada/satmae/temporal/preprocessed/fmow/train/val_62classes.csv \
+    --nb_classes 1000
