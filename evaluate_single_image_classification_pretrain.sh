219c90b85ae504b2018357312b0e03a0ce8f0d71
ada-k <adakibetj@protonmail.com>
2023-10-19 07:54:10 +0000

    single image classification pretrained cpt inference

--- evaluate_single_image_classification_pretrain.sh
+++ evaluate_single_image_classification_pretrain.sh
@@ -1,10 +1,12 @@
 python -m torch.distributed.launch --nproc_per_node=8 \
-    --nnodes=1 --master_port=1234 main_pretrain.py \
+    --nnodes=1 --master_port=1235 main_finetune.py \
+    --batch_size 16 \
     --output_dir /home/ada/satmae/temporal/evaluation/single_image_classification/pretrain \
     --log_dir /home/ada/satmae/temporal/evaluation/single_image_classification/pretrain \
-    --batch_size 16 \
-    --model mae_vit_large_patch16 \
+    --device cpu \
+    --model vit_large_patch16 \
     --resume https://zenodo.org/record/7369797/files/fmow_pretrain.pth \
-    --num_workers 8 --dataset rgb \
-    --train_path /home/ada/satmae/temporal/preprocessed/fmow/train/val_62classes.csv \
-    --device cpu
+    --dist_eval --eval --num_workers 8 --dataset rgb \
+    --train_path /home/ada/satmae/temporal/preprocessed/fmow/train/train_62classes.csv \
+    --test_path /home/ada/satmae/temporal/preprocessed/fmow/train/val_62classes.csv \
+    --nb_classes 1000
